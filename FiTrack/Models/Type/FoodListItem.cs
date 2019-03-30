7f311fbacde9a677696d5d1cf0be04b4a41b6147
stringly <magnificentstringly@gmail.com>
2019-03-30 14:39:10 -0400

    Added FoodListItem and updated FoodViewModel

---
+++ FiTrack/Models/Type/FoodListItem.cs
@@ -0,0 +1,18 @@
+﻿namespace FiTrack.Models.Type
+{
+    public class FoodListItem
+    {
+        public string Name { get; set; }
+        public string Ndbno { get; set; }
+
+        public FoodListItem()
+        {
+        }
+
+        public FoodListItem(Food _food)
+        {
+            Name = _food.Name;
+            Ndbno = _food.ndbno;
+        }
+    }
+}
--- FiTrack/Models/ViewModels/FoodViewModel.cs
+++ FiTrack/Models/ViewModels/FoodViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FiTrack.Models.Type;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,16 @@ namespace FiTrack.Models.ViewModels
 {
     public class FoodViewModel
     {
-        public string Name { get; set; }
-        public string Ndbno { get; set; }
+        public List<FoodListItem> Foods {get;set;}
+
+        public FoodViewModel()
+        {
+            Foods = new List<FoodListItem>();
+        }
+
+        public FoodViewModel(ICollection<Food> _foods)
+        {
+            Foods = _foods.ToList().ConvertAll(x => new FoodListItem(x));
+        }
     }
 }
