8f5ddddc8ab359b9634bc10fb76f8d9f87876359
stringly <magnificentstringly@gmail.com>
2021-08-07 16:07:51 -0400

    New Branch for 2021 USDA database update

---
+++ MacroNewt/Application/Common/Interfaces/IMacroNewtDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacroNewt.Application.Common.Interfaces
+{
+    public interface IMacroNewtDbContext
+    {
+    }
+}
